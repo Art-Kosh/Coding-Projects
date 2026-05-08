a68c352557a54e182fd6c3417425f84049df0d86
Artashes Koshtoyan <153241409+Art-Kosh@users.noreply.github.com>
2026-05-08 10:39:58 -0400

    Update styling and functionality: adjust link margins in calc.css and styles.css; refactor grade calculation logic in script.js

--- Grade Calc/script.js
+++ Grade Calc/script.js
@@ -4,17 +4,22 @@ calcbtn.addEventListener("click", async function () {
   const grades = document.getElementById("grades")
   const gradesArray = grades.value.split(",").map(String);
   const displayAverage = document.getElementById("average");
-  var gradeAverage = 0
+  var gradeTotal = 0
 
   for (const object of gradesArray) {
-    var newObject = object.replace(/%/g, '')
-    gradeAverage += parseFloat(newObject)
+    gradeTotal += parseFloat(object)
   }
+  
   if (gradesArray.length <= 1) {
-    displayAverage.textContent = "Please Enter Grades."
+    displayAverage.textContent = "Please Enter Grades"
+    return
+  }
+  else if (isNaN(gradeTotal)) {
+    displayAverage.textContent = "Not Valid"
     return
   }
   else{
-    displayAverage.textContent = (gradeAverage / gradesArray.length).toFixed(1).toString() + "%"
+    displayAverage.textContent = (gradeTotal / gradesArray.length).toFixed(1).toString() + "%"
+    return
   }
 });
